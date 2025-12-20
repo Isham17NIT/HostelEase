e353c64ca081fd3ed5a96dd2d90744ea660adc4d
Isham17NIT <ishamahuja17@gmail.com>
2025-12-20 14:21:40 +0530

    database connected

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error:", error)
+        throw error
+    })
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server running on port: ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed!!", error)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1> Welcome To Frontend! </h1>
+    </>
+  )
+}
+
+export default App
